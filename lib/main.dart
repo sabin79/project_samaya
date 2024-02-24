2ea5eb843aa2e2d873634073ebf25facd718fe55
sabin79 <dsabin729@gmail.com>
2024-02-24 14:29:37 +0545

    error corrected

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 
 import 'view/bottom_nav_bar.dart';
+import 'view/calender_page.dart';
+import 'view/home_page.dart';
+import 'view/profile_page.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,9 +20,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BottomNavPage(),
+      home: NavigationPage(),
     );
   }
 }
--- lib/view/bottom_nav_bar.dart
+++ lib/view/bottom_nav_bar.dart
@@ -1,54 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconly/iconly.dart';
-import 'package:project_samaya/controller/bottom_nav_controller.dart';
-
+import '../controller/bottom_nav_controller.dart';
 import 'calender_page.dart';
 import 'home_page.dart';
 import 'profile_page.dart';
 
-class BottomNavPage extends StatelessWidget {
-  const BottomNavPage({super.key});
+class NavigationPage extends StatelessWidget {
+  const NavigationPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    BottomNavController bottomNavController = Get.put(BottomNavController());
+    //BOTTOM NAVIGATION BAR CONTROLLER
+    BottomNavController bottomNavBarController = Get.put(BottomNavController());
+
+    //LIST OF DIFFERENT PAGES
     final screens = [
       const HomePage(),
       const CalenderPage(),
-      const ProfilePage(),
+      const ProfilePage()
     ];
 
     return Scaffold(
       body: Obx(
         () => IndexedStack(
-          index: bottomNavController.selectedIndex.value,
-          children: screens,
-        ),
+            index: bottomNavBarController.selectedIndex.value,
+            children: screens),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        showSelectedLabels: true,
-        showUnselectedLabels: true,
-        selectedItemColor: const Color.fromARGB(255, 78, 128, 255),
-        unselectedItemColor: Colors.grey.shade600,
-        currentIndex: bottomNavController.selectedIndex.value,
-        onTap: (index) {
-          bottomNavController.changeIndex(index);
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(IconlyBold.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(IconlyBold.calendar),
-            label: 'Callender',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(IconlyBold.profile),
-            label: 'Profile',
-          ),
-        ],
+      bottomNavigationBar: Obx(
+        () => BottomNavigationBar(
+          selectedItemColor: const Color.fromARGB(255, 78, 128, 255),
+          unselectedItemColor: Colors.grey.shade600,
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          onTap: (index) {
+            bottomNavBarController.changeIndex(index);
+          },
+          currentIndex: bottomNavBarController.selectedIndex.value,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  IconlyBold.home,
+                ),
+                label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  IconlyBold.calendar,
+                ),
+                label: 'Calendar'),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  IconlyBold.profile,
+                ),
+                label: 'Profile'),
+          ],
+        ),
       ),
     );
   }
